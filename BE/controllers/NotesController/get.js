9187d8c2153d322a71906f96bb26fa3191314d99
Siddharth Pareek <siddharth2000pareek@gmail.com>
2025-02-18 22:55:10 +0530

    cleaned the get notes code

--- BE/controllers/NotesController/get.js
+++ BE/controllers/NotesController/get.js
@@ -1,31 +1,42 @@
 const notes = require("../../Model/notes");
+
 const getNotes = async (req, res) => {
   const { isFav, sort } = req.query;
   console.log(sort, isFav);
+
   let findObj = {};
   if (isFav) {
-    findObj = { ...findObj, is_fav: isFav === "true" ? true : false };
+    findObj.is_fav = isFav === "true";
   }
+
   let sortObj = {};
   if (sort) {
     let sortingArray = sort.split(",");
-    console.log(sortingArray);
-    sortingArray.some((item) => {
+
+    const invalid = sortingArray.some((item) => {
       const [field, order] = item.split(":");
-      if (item.includes("asc")) {
-        sortObj[field] = 1;
-      } else if (item.includes("desc")) {
-        sortObj[field] = -1;
-      } else {
-        return res.status(400).send({ error: "Incorrect format for sorting!" });
+
+      if (!field || (order !== "asc" && order !== "desc")) {
+        res.status(400).send({
+          error:
+            "Incorrect format for sorting! Use 'field:asc' or 'field:desc'.",
+        });
+        return true; // Stop execution
       }
+
+      sortObj[field] = order === "asc" ? 1 : -1;
+      return false; // Continue iteration
     });
-    console.log(sortObj);
+
+    if (invalid) return; // Prevent further execution if sorting format is invalid
   }
-  let allNotes = await notes.find(findObj).sort(sortObj);
-  res.status(200).send({ data: allNotes });
+
+  try {
+    let allNotes = await notes.find(findObj).sort(sortObj);
+    res.status(200).send({ data: allNotes });
+  } catch (error) {
+    res.status(500).send({ error: "Internal Server Error" });
+  }
 };
+
 module.exports = { getNotes };
-// created_at:asc,title:desc....
-//[title:desc]
-//
